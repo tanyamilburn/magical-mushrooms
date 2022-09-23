34f1253777cb5440ffb678c14912310ac499a867
Tanya Milburn <tanyamilburn89@gmail.com>
2022-09-23 12:31:41 -0600

    updated images, built out product and category routes/controllers

--- server.js
+++ server.js
@@ -3,18 +3,5 @@ mongoose
   .then(() => console.log("DB connected"))
   .catch((err) => console.log("DB Error => ", err));
 
-  // exports.requireSignin = expressJwt({
-  //   secret: process.env.JWT_SECRET,
-  //   algorithms: ["HS256"], // added later
-  //   userProperty: "auth",
-  // });
-  if (files.photo) {
-    // console.log("FILES PHOTO: ", files.photo);
-    if (files.photo.size > 1000000) {
-      return res.status(400).json({
-        error: "Image should be less than 1mb in size",
-      });
-    }
-    product.photo.data = fs.readFileSync(files.photo.filepath); // change path to filepath
-    product.photo.contentType = files.photo.mimetype; // change typt to mimetype
-  }
+ 
+ 
